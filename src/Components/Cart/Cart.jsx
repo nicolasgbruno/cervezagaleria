596b15a9f01762dc69ad7ca72a9797265f14493b
nicolasgbruno <nicolasg.bruno@gmail.com>
2022-07-26 18:39:34 -0300

    Firebase II

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 import { useCartContext } from '../../Context/CartContext';
 import ItemCart from './ItemCart'
+import { db } from '../../firebase/firebase';
+import { addDoc, collection, serverTimestamp } from 'firebase/firestore';
 
 const Cart = () => {
   const {cart, totalPrice} = useCartContext();
+
+  const order = {
+    buyer: {
+      name: 'Nicolas',
+      email: 'nico@gmail.com',
+      phone: '4652-6598',
+      adress: 'Calle Falsa 123'
+    },
+    items: cart.map(product => ({id: product.id, name: product.name, price: product.price, quantity: product.quantity})),
+    date: serverTimestamp(),
+    total: totalPrice()
+  }
+
+  const handleClick = () => {
+    const ventasCollection = collection(db, 'ventas');
+    addDoc(ventasCollection, order)
+    .then(({id}) => console.log(id))
+  }
 
   if (cart.lenght === 0) {
     return (
@@ -19,6 +39,7 @@ const Cart = () => {
     <> 
     {cart.map(product => <ItemCart key={product.id} product={product}/>)}
     <h2>Total: $ {totalPrice()}</h2>
+    <button onClick={handleClick}>Confirmar compra</button>
     </>
     
   )
